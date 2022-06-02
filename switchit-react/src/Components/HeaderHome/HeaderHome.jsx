e03b51eefc11b71b448fdb7432488871b329d250
LucasGervasi <you@example.com>
2022-06-02 12:02:59 -0300

    HeaderHome, medidas responsive

--- switchit-react/src/Components/HeaderHome/HeaderHome.jsx
+++ switchit-react/src/Components/HeaderHome/HeaderHome.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState } from "react";
 import style from "./HeaderHome.module.css"
 import imagen1 from "./Imagenes HeaderHome/image1.jpg"
 import imagen2 from "./Imagenes HeaderHome/image2.png"
@@ -6,31 +6,46 @@ import imagen3 from "./Imagenes HeaderHome/image3.jpg"
 import imagen4 from "./Imagenes HeaderHome/image4.png"
 
 export default function HeaderHome(){
+    const [active, setActive] = useState("");
+    const sectionStyle = {
+        position: "absolute",
+     height: "60vh",
+     width: "100%",
+     display: "flex",
+    justifyContent: "space-between",
+    overflow: "hidden",
+        backgroundImage: "url(" + active + ")",
+        backgroundSize:"100%" + "100%",
+    }
     return(
         <div className={style.containerHeaderHome}>
-            <section className={style.cajas}>
+            <section style={sectionStyle} >
                     <ul className={style.cajaUl}>
                         <li className={style.cajaLi}>
-                            <a className={style.cajaA}>Caja 1</a>                            
-                            <img className={style.cajaImg} src={imagen1}></img>
+                            <p className={style.cajaA}
+                            onMouseEnter={() => setActive(imagen1)}
+                            >Caja 1</p>                            
                         </li>
                     </ul>
                     <ul className={style.cajaUl}>
                         <li className={style.cajaLi}>
-                            <a className={style.cajaA}>Caja 2</a>
-                            <img className={style.cajaImg} src={imagen2}></img>
+                            <p className={style.cajaA}
+                            onMouseEnter={() => setActive(imagen2)}
+                            >Caja 2</p>
                         </li>
                     </ul>
                     <ul className={style.cajaUl}>
                         <li className={style.cajaLi}>
-                            <a className={style.cajaA}>Caja 3</a>
-                            <img className={style.cajaImg} src={imagen3}></img>
+                        <p className={style.cajaA}
+                            onMouseEnter={() => setActive(imagen3)}
+                            >Caja 3</p>
                         </li>
                     </ul>
                     <ul className={style.cajaUl}>
                         <li className={style.cajaLi}>
-                            <a className={style.cajaA}>Caja 4</a>
-                            <img className={style.cajaImg} src={imagen4}></img>
+                        <p className={style.cajaA}
+                            onMouseEnter={() => setActive(imagen4)}
+                            >Caja 4</p>
                         </li>
                     </ul>
             </section>
